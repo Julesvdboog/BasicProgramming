82ddb3a1daf736a0f689b2bdff0bac581687fdee
Meggylemon1966 <75020802+Meggylemon1966@users.noreply.github.com>
2020-11-25 12:53:37 +0100

    begin code

---
+++ basic.py
@@ -0,0 +1,21 @@
+pip install tweepy
+pip install textblob|
+import tweepy
+from textblob import TextBlob
+
+consumer_key = "bbBt9FB9XcODqo9zoDQXYmIl0"
+consumer_secret = "qgZy4KLEblYPDwvUP08CcGQ23vJig19lbOJHg31Z1DzYPPdArM"
+access_token = "1329013165443739648-Tj3hRpOJiiR4MpuMPR9R7rpastliqz"
+access_token_secret = "lxCtAJNeoaiIe9oWC7MyBuJ6jRyjgS1seCJilFE4Vtjgt"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    
