e221fb36f92e734230d7ca26d3777c86b8c3f435
Meggylemon1966 <75020802+Meggylemon1966@users.noreply.github.com>
2020-11-25 16:26:19 +0100

    dit slaat even nergens op, maar zijn nog dingetjes die we willen doen

--- basic.py
+++ basic.py
@@ -1,7 +1,10 @@
+from __future__ import unicode_literals
+from pattern.en import sentiment as sentiment_en
 import tweepy
 from textblob import TextBlob
 import re
 from collections import Counter
+
 
 consumer_key = "bbBt9FB9XcODqo9zoDQXYmIl0"
 consumer_secret = "qgZy4KLEblYPDwvUP08CcGQ23vJig19lbOJHg31Z1DzYPPdArM"
@@ -20,14 +23,24 @@ all_tweets = tweepy.Cursor(api.search, q='Trump', lang = "en").items(100)
 for tweet in all_tweets:
     tweets.append(tweet.text)
     
+    
+ 
 def Tokenizer(zin):
     nieuwe_zin = re.findall("\w+", str(zin))
-    ignore = ['to','is', 'in']
+    ignore = ['to','is','in']
     cleaned_zin = [w.lower() for w in nieuwe_zin if w not in ignore]
     nieuw = Counter(cleaned_zin)
     return nieuw
     
-    
 
 print(Tokenizer(tweets))
+
+STOP_WORDS = set(tweets)
+
+for tweet in all_tweets:
+    tweets.count(tweet.text)
+
+for tweet in all_tweets:
+    polarity, subjectivity = sentiment_en(tweets)
+    print(polarity, subjectivity)
 
